4fb123a5e5252dc60a66c7f417ff8859d86436ab
ria-nanuda <kbr@widaehan.com>
2022-01-12 16:38:39 +0900

    test server

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+app.set("port", port);
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+app.listen(port, () => console.log("Listening on", port));
+module.exports = app;
